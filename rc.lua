91edee63b35f3fe7c07e75e1623e08bb350eb679
ia <isaac.ardis@gmail.com>
2018-07-13 07:10:38 -0400

    tiler orderer

--- rc.lua
+++ rc.lua
@@ -109,11 +109,11 @@ end
 awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5" }
 awful.layout.layouts = {
-    awful.layout.suit.floating,
-    awful.layout.suit.tile,
-    awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
+    awful.layout.suit.tile,
+    awful.layout.suit.tile.left,
+    awful.layout.suit.floating,
     --awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral,
