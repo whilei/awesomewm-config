6faa875fd8195e5e4e377274e8c4c51b9f3542cd
ia <isaac.ardis@gmail.com>
2019-12-27 15:42:41 -0600

    adds extrat layout deafult opt

--- rc.lua
+++ rc.lua
@@ -114,14 +114,14 @@ awful.layout.layouts = {
     awful.layout.suit.tile.top,
     awful.layout.suit.tile,
     awful.layout.suit.tile.left,
-    awful.layout.suit.floating,
-    --awful.layout.suit.fair,
+    --awful.layout.suit.floating,
+    awful.layout.suit.fair,
     --awful.layout.suit.fair.horizontal,
     --awful.layout.suit.spiral,
     --awful.layout.suit.spiral.dwindle,
     --awful.layout.suit.max,
     --awful.layout.suit.max.fullscreen,
-    --awful.layout.suit.magnifier,
+    awful.layout.suit.magnifier,
     --awful.layout.suit.corner.nw,
     --awful.layout.suit.corner.ne,
     --awful.layout.suit.corner.sw,
@@ -406,8 +406,8 @@ globalkeys = my_table.join(
               {description = "dropdown application", group = "launcher"}),
 
     -- Widgets popups
-    awful.key({ altkey, }, "c", function () lain.widget.calendar.show(7) end,
-              {description = "show calendar", group = "widgets"}),
+    -- awful.key({ altkey, }, "c", function () lain.widget.calendar.show(7) end,
+              -- {description = "show calendar", group = "widgets"}),
     awful.key({ altkey, }, "h", function () if beautiful.fs then beautiful.fs.show(7) end end,
               {description = "show filesystem", group = "widgets"}),
 
