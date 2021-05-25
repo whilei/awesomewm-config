a662800be521266c06cd15b6dddf2c7b2bb9a909
ia <isaac.ardis@gmail.com>
2021-05-25 07:55:50 -0500

    set wallpaper from cli
    Signed-off-by: ia <isaac.ardis@gmail.com>

---
+++ awesome-client-wallpaper.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# https://www.reddit.com/r/awesomewm/comments/h0xorw/dynamic_wallpapers_in_awesome/
+
+# awesome-client "local gears = require('gears'); gears.wallpaper.maximized('/home/ia/.config/awesome/themes/ia/walls/solidcolor_black.png', 1, true)"
+# awesome-client "local gears = require('gears'); gears.wallpaper.maximized('/home/ia/.config/awesome/themes/ia/walls/wall4.png', 1, true)"
+# awesome-client "local gears = require('gears'); gears.wallpaper.maximized('/home/ia/.config/awesome/themes/ia/walls/wall4.png', 1, true)"
+# awesome-client "local gears = require('gears'); gears.wallpaper.maximized('/home/ia/.config/awesome/themes/ia/walls/wall4.jpg', 1, true)"
+# awesome-client "local gears = require('gears'); gears.wallpaper.maximized('/home/ia/.config/awesome/themes/ia/walls/munkacsy_the-music-room.jpg', 1, true)"
+# awesome-client "local gears = require('gears'); gears.wallpaper.centered('/home/ia/.config/awesome/themes/ia/walls/munkacsy_the-music-room.jpg', 1, true)"
+# awesome-client "local gears = require('gears'); gears.wallpaper.centered('/home/ia/.config/awesome/themes/ia/walls/munkacsy_the-music-room.jpg', 1, "#000000", true)"
+# awesome-client "local gears = require('gears'); gears.wallpaper.centered('/home/ia/.config/awesome/themes/ia/walls/munkacsy_the-music-room.jpg', 1, nil, true)"
+# awesome-client "local gears = require('gears'); gears.wallpaper.centered('/home/ia/.config/awesome/themes/ia/walls/munkacsy_the-music-room.jpg', 1)"
+
+awesome-client "local gears = require('gears'); gears.wallpaper.fit('/home/ia/.config/awesome/themes/ia/walls/munkacsy_the-music-room.jpg', 1)"
