d2e12c7ced58430638e0af05f216f04c1431142c
ia <isaac.ardis@gmail.com>
2021-05-27 05:37:57 -0500

    :floppy_disk:
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- themes/ia/theme.lua
+++ themes/ia/theme.lua
@@ -885,86 +885,91 @@ function theme.at_screen_connect(s)
     --    w:add(l)
     --end
 
-    ----- Common update method.
-    ---- @param w The widget.
-    ---- @tab buttons
-    ---- @func label Function to generate label parameters from an object.
-    ----   The function gets passed an object from `objects`, and
-    ----   has to return `text`, `bg`, `bg_image`, `icon`.
-    ---- @tab data Current data/cache, indexed by objects.
-    ---- @tab objects Objects to be displayed / updated.
-    --function my_commonlist_update(w, buttons, label, data, objects)
-    --    -- update the widgets, creating them if needed
-    --    w:reset()
-    --    for i, o in ipairs(objects) do
-    --        local cache = data[o]
-    --        local ib, tb, bgb, tbm, ibm, l
-    --        --local ib, bgb, tbm, ibm, l
-    --        if cache then
-    --            ib = cache.ib
-    --            tb = cache.tb
-    --            bgb = cache.bgb
-    --            tbm = cache.tbm
-    --            ibm = cache.ibm
-    --        else
-    --            ib = wibox.widget.imagebox()
-    --            tb = wibox.widget.textbox()
-    --            bgb = wibox.container.background()
-    --            tbm = wibox.container.margin(tb, dpi(4), dpi(4))
-    --            ibm = wibox.container.margin(ib, dpi(4))
-    --            l = wibox.layout.fixed.horizontal()
-    --
-    --            -- All of this is added in a fixed widget
-    --            l:fill_space(true)
-    --            l:add(ibm)
-    --            l:add(tbm)
-    --
-    --            -- And all of this gets a background
-    --            bgb:set_widget(l)
-    --
-    --            bgb:buttons(common.create_buttons(buttons, o))
-    --
-    --            data[o] = {
-    --                ib  = ib,
-    --                tb  = tb,
-    --                bgb = bgb,
-    --                tbm = tbm,
-    --                ibm = ibm,
-    --            }
-    --        end
-    --
-    --        local text, bg, bg_image, icon, args = label(o, tb)
-    --        args = args or {}
-    --
-    --        -- The text might be invalid, so use pcall.
-    --        --text = ""
-    --        if text == nil or text == "" then
-    --            tbm:set_margins(0)
-    --        else
-    --            if not tb:set_markup_silently(text) then
-    --                tb:set_markup("<i>&lt;Invalid text&gt;</i>")
-    --            end
-    --        end
-    --        tb:set_markup(" ")
-    --        bgb:set_bg(bg)
-    --        if type(bg_image) == "function" then
-    --            -- TODO: Why does this pass nil as an argument?
-    --            bg_image = bg_image(tb,o,nil,objects,i)
-    --        end
-    --        bgb:set_bgimage(bg_image)
-    --        if icon then
-    --            ib:set_image(icon)
-    --        else
-    --            ibm:set_margins(0)
-    --        end
-    --
-    --        bgb.shape              = args.shape
-    --        bgb.shape_border_width = args.shape_border_width
-    --        bgb.shape_border_color = args.shape_border_color
-    --
-    --        w:add(bgb)
-    --    end
-    --end
+    --- Common update method.
+    -- @param w The widget.
+    -- @tab buttons
+    -- @func label Function to generate label parameters from an object.
+    --   The function gets passed an object from `objects`, and
+    --   has to return `text`, `bg`, `bg_image`, `icon`.
+    -- @tab data Current data/cache, indexed by objects.
+    -- @tab objects Objects to be displayed / updated.
+    function my_commonlist_update(w, buttons, label, data, objects)
+        -- update the widgets, creating them if needed
+        w:reset()
+        for i, o in ipairs(objects) do
+            local cache = data[o]
+            local ib, tb, bgb, tbm, ibm, l
+            --local ib, bgb, tbm, ibm, l
+            if cache then
+                ib = cache.ib
+                tb = cache.tb
+                bgb = cache.bgb
+                tbm = cache.tbm
+                ibm = cache.ibm
+            else
+                ib = wibox.widget.imagebox()
+                tb = wibox.widget.textbox()
+                bgb = wibox.container.background()
+                tbm = wibox.container.margin(tb, dpi(4), dpi(4))
+                ibm = wibox.container.margin(ib, dpi(4))
+                l = wibox.layout.fixed.horizontal()
+
+                -- All of this is added in a fixed widget
+                l:fill_space(true)
+                l:add(ibm)
+                l:add(tbm)
+
+                -- And all of this gets a background
+                bgb:set_widget(l)
+
+                bgb:buttons(common.create_buttons(buttons, o))
+
+                data[o] = {
+                    ib  = ib,
+                    tb  = tb,
+                    bgb = bgb,
+                    tbm = tbm,
+                    ibm = ibm,
+                }
+            end
+
+            local text, bg, bg_image, icon, args = label(o, tb)
+            args = args or {}
+
+            -- IA
+            -- This is my special additional
+            -- to REMOVE TEXT (LEAVING ONLY ICON)
+            -- if the task does indeed have an icon.
+            if icon then text = "" end
+
+            -- The text might be invalid, so use pcall.
+            if text == nil or text == "" then
+                tbm:set_margins(0)
+            else
+                if not tb:set_markup_silently(text) then
+                    tb:set_markup("<i>&lt;Invalid text&gt;</i>")
+                end
+            end
+            --tb:set_markup(" ")
+            bgb:set_bg(bg)
+            if type(bg_image) == "function" then
+                -- TODO: Why does this pass nil as an argument?
+                bg_image = bg_image(tb,o,nil,objects,i)
+            end
+            bgb:set_bgimage(bg_image)
+            if icon then
+                ib:set_image(icon)
+            else
+                ibm:set_margins(0)
+            end
+
+            bgb.shape              = args.shape
+            bgb.shape_border_width = args.shape_border_width
+            bgb.shape_border_color = args.shape_border_color
+
+            w:add(bgb)
+        end
+    end
 
     -- awful.widget.tasklist()
     s.mytasklist = awful.widget.tasklist(
@@ -973,9 +978,15 @@ function theme.at_screen_connect(s)
         awful.util.tasklist_buttons, -- buttons
         nil, -- style
         list_update -- update function
-        --myupdate
-        --my_commonlist_update
     )
+    -- This works. Just commented because I'm not using icon-only task list for the slim wibar now.
+    --s.mytasklist_slim = awful.widget.tasklist(
+    --    s, -- screen
+    --    awful.widget.tasklist.filter.currenttags, -- filter
+    --    awful.util.tasklist_buttons, -- buttons
+    --    nil, -- style
+    --    my_commonlist_update
+    --)
 
     -- Create the wibox
     -- opacity isnt affected even with the table keybecause you need to add the two hex codes to the bg, eg.  '.. "aa"'
@@ -1012,15 +1023,12 @@ function theme.at_screen_connect(s)
         --height = 120, -- 18
         --width = 14,
 
-        --bg = "#0D001E", -- theme.bg_normal, -- .. "00",
-        --fg = theme.fg_normal,
-        -- bg = "#00000000", -- theme.bg_normal, -- .. "00",
         bg = theme.tasklist_bg_normal,
 
         ontop = true,
         type = "dock", -- "toolbar", -- ,
 
-        input_passthrough = true,
+        input_passthrough = true, -- noop, btw
     })
 
     s.mywibox_worldtimes = awful.wibar({
@@ -1124,7 +1132,10 @@ function theme.at_screen_connect(s)
             spr,
 
             s.mylayoutbox,
+            spr,
 
+            --s.mytasklist_slim,
+            --spr,
         },
     }
 
