94d49425277ecbfa60f81482a26cb80b8d86146a
ia <isaac.ardis@gmail.com>
2022-12-21 06:16:10 -0800

    :floppy_disk:
    Signed-off-by: ia <isaac.ardis@gmail.com>

--- layout-swen/init.lua
+++ layout-swen/init.lua
@@ -47,87 +47,56 @@ local swen   = {
 }
 
 local function arrange(p, layout)
-	local wa  = p.workarea
-	local cls = p.clients
-	local t   = p.tag or capi.screen[p.screen].selected_tag
+	local wa                       = p.workarea
+	local cls                      = p.clients
+	local t                        = p.tag or capi.screen[p.screen].selected_tag
 
-	-- 1: South
-	-- 2: West
-	-- 3: East
-	-- 4: North
+	local south, west, east, north = 1, 2, 3, 4
 
-	if #cls == 0 then
-		return
+	local mwfact                   = t.master_width_factor
+	local main_width               = math.floor(wa.width * mwfact)
+	local main_height              = (#cls < 4) and math.floor(wa.height * 3 / 4) or math.floor(wa.height / 2)
+
+	local slave_width_ew           = (wa.width - main_width) / 2 -- sides a,b
+	local slave_width_n            = main_width
+
+	local wa_bottom                = wa.y + wa.height
+	local wa_right                 = wa.x + wa.width
+
+	for i, cl in ipairs(cls) do
+		if i == south then
+			p.geometries[cl] = {
+				height = main_height,
+				width  = main_width,
+				y      = wa_bottom - main_height,
+				x      = wa.x + slave_width_ew,
+			}
+		elseif i == west then
+			p.geometries[cl] = {
+				height = wa.height,
+				width  = slave_width_ew,
+				y      = wa.y,
+				x      = wa.x,
+			}
+		elseif i == east then
+			p.geometries[cl] = {
+				height = wa.height,
+				width  = slave_width_ew,
+				y      = wa.y,
+				x      = wa_right - slave_width_ew,
+			}
+		elseif i == north then
+			p.geometries[cl] = {
+				height = wa.height - main_height,
+				width  = main_width,
+				y      = wa.y,
+				x      = wa.x + slave_width_ew,
+			}
+		else
+			-- What to do with the rest of the clients?
+
+		end
 	end
-
-	-- ac is a client
-	-- South:
-	local south_client         = cls[1]
-	p.geometries[south_client] = {
-		height = wa.height / 3 * 2,
-		width  = wa.width / 3 * 2,
-		y      = wa.y + wa.height / 3,
-		x      = wa.x + wa.width / 3 / 2,
-	}
-	if #cls == 1 then
-		return
-	end
-
-	-- There are more than 1 clients.
-
-	-- Make 'south' positioned client (the first one)
-	-- skinnier than it would be if it were the only client.
-	-- This allows room for the West client.
-	p.geometries[south_client] = {
-		height = wa.height / 3 * 2, -- stays the same
-		width  = wa.width / 2,
-		y      = wa.y + wa.height / 3,
-		x      = wa.x + wa.width / 2 / 2,
-	}
-
-	-- West:
-	local west_client          = cls[2]
-	p.geometries[west_client]  = {
-		height = wa.height,
-		width  = wa.width / 2 / 2,
-		y      = wa.y,
-		x      = wa.x,
-	}
-	if #cls == 2 then
-		return
-	end
-
-	-- East:
-	local east_client         = cls[3]
-	p.geometries[east_client] = {
-		height = wa.height,
-		width  = wa.width / 2 / 2,
-		y      = wa.y,
-		x      = wa.x + wa.width / 2 / 2 * 3,
-	}
-	if #cls == 3 then
-		return
-	end
-
-	-- North:
-	local north_client         = cls[4]
-	p.geometries[north_client] = {
-		height = wa.height / 2,
-		width  = wa.width / 2,
-		y      = wa.y,
-		x      = wa.x + wa.width / 2 / 2,
-	}
-	p.geometries[south_client] = {
-		height = wa.height / 2,
-		width  = wa.width / 2,
-		y      = wa.y + wa.height / 2,
-		x      = wa.x + wa.width / 2 / 2,
-	}
-
-	if #cls >= 4 then
-		return
-	end
-
 end
 
 function swen.arrange(p)
